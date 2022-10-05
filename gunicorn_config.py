0dffc26e5ce272585f5378f3e8e2813d6dab2305
Nigel Faria <nigel.faria@gmail.com>
2022-10-05 15:59:19 +0100

    Gunicorn config

---
+++ gunicorn_config.py
@@ -0,0 +1,22 @@
+"""Gunicorn config file"""
+# Django WSGI application path in pattern MODULE_NAME:VARIABLE_NAME
+
+wsgi_app = "speedmathsdrills.wsgi:application"
+
+loglevel = "error"
+
+workers =2
+
+bind = "0.0.0.0:8000"
+
+reload = True
+
+accesslog = "/var/log/gunicorn/speedmathsdrills.com.log"
+errorlog = "/var/log/gunicorn/speedmathsdrills.com.log"
+
+
+capture_output = True
+
+pidfile = "/var/run/gunicorn.pid"
+
+daemon = False
