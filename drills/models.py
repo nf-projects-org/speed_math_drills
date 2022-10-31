037b6201d7e029e9f97817d013b1c0aefdc00e34
Nigel Faria <nigel.faria@gmail.com>
2022-10-31 19:21:20 +0000

    basic working of drills and generation

--- drills/models.py
+++ drills/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from accounts.models import SmdUser
+import uuid
 
 OPERATION_CHOICES = ( 
-    (1,'+'),(2,'-'),(3,'x'),(4,'÷'),(5,'^'), (6,'√a')
+    ('+','Addition'),('-','Subtraction'),('x','Multiplication'),('÷','Division'),('^','Exponent'), ('√a','Roots')
 )
-OPERATION_CHOICES_DICT = dict(OPERATION_CHOICES)
 
 CATEGORY_CHOICES = (
     (1,'Competitive Exam'),(2,'Student'), (3, 'Neurobics'), (4, 'Just Playing'), (5, 'Consultant')
@@ -12,11 +12,12 @@ CATEGORY_CHOICES = (
 CATEGORY_CHOICES_DICT = dict(CATEGORY_CHOICES)
 
 class UniversalDrill(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     first_param_start = models.PositiveIntegerField(verbose_name='Start1', default=1,)
     first_param_end = models.PositiveIntegerField(verbose_name='End1', default=30, )
     second_param_start = models.PositiveIntegerField(verbose_name='Start2', default=1,)
     second_param_end = models.PositiveIntegerField(verbose_name='End2', default=30, )
-    operation = models.PositiveSmallIntegerField(choices=OPERATION_CHOICES, default=3)
+    operation = models.CharField(choices=OPERATION_CHOICES, max_length=3, default='x')
     name = models.CharField(max_length=50, default='')
     category = models.PositiveSmallIntegerField(choices=CATEGORY_CHOICES,default=1)
 
@@ -25,16 +26,17 @@ class UniversalDrill(models.Model):
         verbose_name_plural = 'Universal Drills'
     
     def __str__(self):
-        return self.name + " | " + str( CATEGORY_CHOICES_DICT[self.category]) + " [" + str(self.first_param_start) +", "+ str(self.first_param_end) +"] "  + str(OPERATION_CHOICES_DICT[self.operation]) \
+        return self.name + " | " + str( CATEGORY_CHOICES_DICT[self.category]) + " [" + str(self.first_param_start) +", "+ str(self.first_param_end) +"] "  + str(self.operation) \
         + " [" + str(self.second_param_start) +", "+ str(self.second_param_end) +"]" 
 
 class UserDrill(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(SmdUser, models.CASCADE, related_name='Owner', )
     first_param_start = models.PositiveIntegerField(verbose_name='Start1', default=1,)
     first_param_end = models.PositiveIntegerField(verbose_name='End1', default=30, )
     second_param_start = models.PositiveIntegerField(verbose_name='Start2', default=1,)
     second_param_end = models.PositiveIntegerField(verbose_name='End2', default=30, )
-    operation = models.PositiveSmallIntegerField(choices=OPERATION_CHOICES, default=3)
+    operation = models.CharField(choices=OPERATION_CHOICES, max_length=3, default='x')
     name = models.CharField(max_length=50, default='')
     category = models.CharField(max_length=50, default='')
 
@@ -43,5 +45,5 @@ class UserDrill(models.Model):
         verbose_name_plural = 'User Drills'
     
     def __str__(self):
-        return self.name + " | " + self.category + " [" + str(self.first_param_start) +", "+ str(self.first_param_end) +"] "  + str(OPERATION_CHOICES_DICT[self.operation]) \
+        return self.name + " | " + self.category + " [" + str(self.first_param_start) +", "+ str(self.first_param_end) +"] "  + str(self.operation) \
         + " [" + str(self.second_param_start) +", "+ str(self.second_param_end) +"]" 
--- static/js/main.js
+++ static/js/main.js
@@ -1,2 +1,105 @@
+//Initialize state
 
+//Calculations Display
+var stateCalculationDisplay = {};
+stateCalculationDisplay.first = document.body.querySelector(".display_first");
+stateCalculationDisplay.second = document.body.querySelector(".display_second");
+stateCalculationDisplay.result = document.body.querySelector(".display_result");
+stateCalculationDisplay.update = function(first=0, second=0, result=0){
+    stateCalculationDisplay.first.innerHTML = first;
+    stateCalculationDisplay.second.innerHTML = second;
+    stateCalculationDisplay.result.innerHTML = result;
+}
+
+
+//Calculations Processing
+var stateCalculations = {};
+stateCalculations.first=[];
+stateCalculations.second=[];
+stateCalculations.current_counter=0;
+stateCalculations.generate = function(){
+    state_calculations_first =[];
+    state_calculations_second=[];
+    state_calculations_current_counter=0;
+    let counter = 0;
+    for(let i = parseInt( stateDrillDetailsDisplay.first_param_start.innerHTML); i<= parseInt(stateDrillDetailsDisplay.first_param_end.innerHTML); i++){
+        for (let j= parseInt(stateDrillDetailsDisplay.second_param_start.innerHTML); j<= parseInt(stateDrillDetailsDisplay.second_param_end.innerHTML); j++){
+            state_calculations_first[counter] = i;
+            state_calculations_second[counter] = j;
+            counter++;
+        }
+    }
+};
+
+
+//Drill Details
+var stateDrillDetailsDisplay = {};
+stateDrillDetailsDisplay.update = function(){
+    stateDrillDetailsDisplay.element = document.querySelector(".drill-details-display")
+    stateDrillDetailsDisplay.first_param_start = stateDrillDetailsDisplay.element.querySelector(".drill_first_param_start");
+    stateDrillDetailsDisplay.first_param_end = stateDrillDetailsDisplay.element.querySelector(".drill_first_param_end");
+    stateDrillDetailsDisplay.operation = stateDrillDetailsDisplay.element.querySelector(".drill_operation") ;
+    stateDrillDetailsDisplay.second_param_start = stateDrillDetailsDisplay.element.querySelector(".drill_second_param_start");
+    stateDrillDetailsDisplay.second_param_end = stateDrillDetailsDisplay.element.querySelector(".drill_second_param_end");
+ };
+stateDrillDetailsDisplay.play =  function(){
+    //Read the Drill
+    if (stateDrillDetailsDisplay.element.getAttribute('data-uuid') === "dummy"){
+        return;
+    }
+    stateDrillDetailsDisplay.update();    
+
+    //Read the Shuffle buttons
+
+    //Generate the sequence of calculations
+    stateCalculations.generate();
+    //Update the display with the first calculation
+    stateCalculationDisplay.update(state_calculations_first[0],state_calculations_second[0])
+}
+
+
+//Drill Selector
+
+var stateDrillSelector = {};
+stateDrillSelector.action = function(){
+    // Has to be initialised after HTMX swap
+    stateDrillSelector.element = document.getElementById('drill-selector');
+    stateDrillSelector.selectedDrill = stateDrillSelector.element.value;
+    stateDrillSelector.selectedDrillUuid = null;
+    stateDrillDetailsDisplay.update();
+
+    for (let element of stateDrillSelector.element.children){
+        if (element.value === stateDrillSelector.selectedDrill){
+            stateDrillSelector.selectedDrillUuid = element.getAttribute("data-uuid");
+            stateDrillDetailsDisplay.element.setAttribute("data-uuid", stateDrillSelector.selectedDrillUuid);  
+            //Actual drill selected so lets remove the Drills dummy holder
+            for(let i=0; i < stateDrillSelector.element.length;i++){
+                if(stateDrillSelector.element.options[i].value==='Drills'){
+                    stateDrillSelector.element.remove(i);
+                    break;
+                }
+            }
+            break;
+        }
+    }
+    for (let element of document.querySelectorAll(".drill-details")){    
+        if (element.getAttribute("data-uuid") === stateDrillSelector.selectedDrillUuid){
+            stateDrillDetailsDisplay.first_param_start.innerHTML = element.dataset.drill_first_param_start;
+            stateDrillDetailsDisplay.first_param_end.innerHTML = element.dataset.drill_first_param_end;
+            stateDrillDetailsDisplay.operation.innerHTML = element.dataset.drill_operation;
+            stateDrillDetailsDisplay.second_param_start.innerHTML = element.dataset.drill_second_param_start;
+            stateDrillDetailsDisplay.second_param_end.innerHTML = element.dataset.drill_second_param_end;
+            break;
+        }
+    }
+}    
   
+
+//Add event listener for HTMX swap
+document.body.addEventListener('htmx:afterSwap', function (evt){
+    if (evt.target.matches(".drill-select")){
+        document.getElementById('drill-selector').addEventListener("change", stateDrillSelector.action);
+        document.body.querySelector(".drill_play_button").addEventListener('click',stateDrillDetailsDisplay.play);
+    }
+});
+
