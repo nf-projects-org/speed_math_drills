9725fb75c9adef90cc9763c11e563a3a4d4d7c82
Nigel Faria <nigel.faria@gmail.com>
2022-11-08 22:00:36 +0000

    Keypad and state

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,25 @@
+"use strict";
 function isNumeric(n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
 document.addEventListener('alpine:init', () => {
+    Alpine.store('stateSessionChoice',{
+        reset: function(){
+            console.log("stateSessionChoice:Reset");
+            let sd = Alpine.store('stateDrillDetailsDisplay');
+            let scd = Alpine.store('stateCalculationDisplay');
+            let sk = Alpine.store('stateKeypad');
+            let sc = Alpine.store('stateCalculations');
+
+            sd.reset();
+            scd.reset();
+            sk.reset();
+            sc.reset();
+            
+        },
+    });
+
     Alpine.store('stateCalculationDisplay',{
         first:0,
         displayFirst:true,
@@ -12,13 +29,26 @@ document.addEventListener('alpine:init', () => {
         displaySecond:true,
         result:'',
         isResultCorrect:false,
+        displayMessage:false,
+        disabled:true,
+        reset: function(){
+            let sc = Alpine.store('stateCalculationDisplay');
+            sc.first = 0; sc.displayFirst = true; sc.operation = 'x'; sc.displayOperation = true; sc.second =0;
+            sc.displaySecond = true; sc.result = ''; sc.isResultCorrect = false; sc.displayMessage = false; sc.disabled = true;
+        },
         checkResult : function(){
             let sc = Alpine.store('stateCalculations');
             sc.checkResult();      
         },
+        skip: function(){
+            let sc = Alpine.store('stateCalculations');
+            sc.skip();
+        },
+
 
     });
     Alpine.store('stateDrillDetailsDisplay', {
+        disabled: true,
         first:2,
         isFirstReadonly:false,
         isFirstValid:true,
@@ -28,6 +58,14 @@ document.addEventListener('alpine:init', () => {
         second_end:15,
         isSecondEndValid:true,
         random:true,
+        precision:3,
+        displayPrecision:false,
+        isPrecisionValid:true,
+        reset: function(){
+            let sd = Alpine.store('stateDrillDetailsDisplay');
+            sd.disabled = true; sd.first = 2; sd.isFirstReadonly = false; sd.isFirstValid = true; sd.drill=''; sd.second_start=1; sd.second_end=15;
+            sd.isSecondStartValid = true; sd.isSecondEndValid = true; sd.random = true; sd.precision = 3; sd.displayPrecision = false; sd.isPrecisionValid = true;
+        },
         processChange: function(source){
             let stateDrillDetailsDisplay = Alpine.store('stateDrillDetailsDisplay');
             if(stateDrillDetailsDisplay.validate(source)){
@@ -58,24 +96,38 @@ document.addEventListener('alpine:init', () => {
                 validated = false;
                 sd.isSecondEndValid = false;
             }
+            if(isNumeric(sd.precision) && sd.precision > 0 && sd.precision < 7 ){
+                sd.isPrecisionValid = true;
+            }
+            else{
+                validated = false;
+                sd.isPrecisionValid = false;
+            }
             return validated;
         },
         updateDrills: function(){
             let sd = Alpine.store('stateDrillDetailsDisplay');
+            let scd = Alpine.store('stateCalculationDisplay');
+            let sk = Alpine.store('stateKeypad');
+
+            sd.disabled = false;
+            scd.disabled = false;
+            sk.disabled = false;
+
             if(sd.drill === 'Tables'){
-                sd.setState(2,false, 'x', 1, 30, true, true, true);
+                sd.setState(2,false, 'x', 1, 30, true, true, true,false);
             }
             else if(sd.drill === 'Squares'){
-                sd.setState(2,true, '²', 1, 20, true, true, false );
+                sd.setState(2,true, '²', 1, 20, true, true, false, false );
             }
             else if(sd.drill === 'Cubes'){
-                sd.setState(3,true, '³', 1, 20, true, true, false);
+                sd.setState(3,true, '³', 1, 20, true, true, false, false);
             }
             else if(sd.drill === 'Fractions'){
-                sd.setState(1,true, '÷', 1, 30, true, true, true);
+                sd.setState(1,true, '÷', 1, 30, true, true, true, true);
             }
             else if(sd.drill === 'Sq. Roots'){
-                sd.setState(2,true, '√', 1, 15, true, false, true);
+                sd.setState(2,true, '√', 1, 15, true, false, true, true);
             }
             sd.start();
         },
@@ -84,11 +136,11 @@ document.addEventListener('alpine:init', () => {
             sc.generate();
             sc.getNext();
         },
-        setState: function(first, isFirstReadonly, operation, second_start, second_end, random, displayFirstCalc, displaySecondCalc){
+        setState: function(first, isFirstReadonly, operation, second_start, second_end, random, displayFirstCalc, displaySecondCalc, displayPrecision){
             let sd = Alpine.store('stateDrillDetailsDisplay');
             let scd = Alpine.store('stateCalculationDisplay');
             sd.first = first; sd.isFirstReadonly = isFirstReadonly;  sd.second_start = second_start; sd.second_end = second_end; sd.random = random;
-            scd.operation=operation; scd.displayFirst = displayFirstCalc; scd.displaySecond = displaySecondCalc;
+            scd.operation=operation; scd.displayFirst = displayFirstCalc; scd.displaySecond = displaySecondCalc; sd.displayPrecision = displayPrecision;
         }, 
 
     });
@@ -105,11 +157,28 @@ document.addEventListener('alpine:init', () => {
         ]),
         operationsMap : new Map([
             ['Tables', (a,b) => a*b],
-            ['Squares', (a,b) => Math.pow(a,2)],
-            ['Cubes', (a,b) => Math.pow(a,3)],
-            ['Fractions', (a,b) => 1/b],
-            ['Sq. Roots', (a,b) => Math.pow(a,1/2)],
+            ['Squares', (a,b) => Math.pow(b,2)],
+            ['Cubes', (a,b) => Math.pow(b,3)],
+            ['Fractions', (a,b) => { 
+                let sc = Alpine.store('stateCalculations');
+                return sc.roundToPrecision(1/b);
+            }],
+            ['Sq. Roots', (a,b) => {
+                let sc = Alpine.store('stateCalculations');
+                return sc.roundToPrecision(Math.pow(b,1/2));
+            }],
         ]),
+        reset: function(){
+            let sc = Alpine.store('stateCalculations');
+            sc.calculationList = [];
+            sc.currentCounter = -1;
+            sc.maxCalculations = 0;
+        },
+        roundToPrecision: function(arg){
+            let precision = Alpine.store('stateDrillDetailsDisplay').precision;
+            let multiple = Math.pow(10,precision);
+            return Math.round((multiple) * arg)/multiple; 
+        },
         shuffle: function(array){
             var shuffled = [...array];
             for (let i = shuffled.length -1; i > 0; i--){
@@ -150,109 +219,87 @@ document.addEventListener('alpine:init', () => {
                 scd.isResultCorrect = false;
             }else
             {
-                scd.result = 'Drill Complete';
+                //Display confirmation
+                scd.displayMessage = true;
+                setTimeout(()=>scd.displayMessage=false,2000);
+                //Restart drill
+                sd.start();
             }
         },
         checkResult: function(){
             let sc = Alpine.store('stateCalculations');
             let sd = Alpine.store('stateDrillDetailsDisplay');
             let scd = Alpine.store('stateCalculationDisplay');
-
+            console.log("#checkResult")
+            console.log(scd.result);
+            console.log(typeof(scd.result))
+            console.log("checkResult#")
             let resultOp = sc.operationsMap.get(sd.drill);
             let result = resultOp(sc.calculationList[sc.currentCounter][0],sc.calculationList[sc.currentCounter][1]);
             if (scd.result === result){
                 scd.isResultCorrect = true;
-                setTimeout(sc.getNext, 800); 
+                setTimeout(sc.getNext, 1000); 
+            }
+        },
+        skip: function(){
+            let sc = Alpine.store('stateCalculations');
+            let sd = Alpine.store('stateDrillDetailsDisplay');
+            let scd = Alpine.store('stateCalculationDisplay');
+            // display the correct result if possible
+            if (sc.currentCounter < sc.maxCalculations){
+                let resultOp = sc.operationsMap.get(sd.drill);
+                let result = resultOp(sc.calculationList[sc.currentCounter][0],sc.calculationList[sc.currentCounter][1]);
+                scd.result = result;
+                setTimeout(sc.getNext, 1000); 
             }
         },
         
     });
-     
-});
+    Alpine.store('stateKeypad',{
+        displayKeypad:true,
+        disabled:true,
+        reset: function(){
+            let sk = Alpine.store('stateKeypad');
+            sk.displayKeypad = true;
+            sk.disabled = true;
+        },
+        keypadListener: function(key){
+            let scd = Alpine.store('stateCalculationDisplay');
+            let keyNumber=0;
+            let keyString = '';
+            let resultString = scd.result.toString();
+            let isBlank = false;
+            if(scd.result === 0 || scd.result === ''){
+                isBlank = true;
+            }
 
-
-stateDrillDetailsDisplay.next = function() {
-    //Is a proper drill loaded else return
-    if (stateDrillDetailsDisplay.element.getAttribute('data-uuid') === "dummy"){
-        return;
-    }
-    if (stateCalculations.isDrillOver()){return;}
-
-    let current_result = stateCalculations.getCurrentResult();
-    let current_display_result =  parseInt(stateCalculationDisplay.result.innerHTML);
-    if( current_display_result === current_result){
-        if(!stateCalculations.nextCalculation()){
-            return;
-        }
-        let currentCalc = stateCalculations.getCurrentCalculation();    
-        stateCalculationDisplay.updateFirst(currentCalc.first);
-        stateCalculationDisplay.updateSecond(currentCalc.second);
-        stateCalculationDisplay.updateResult('?');
-    } else{
-        stateCalculationDisplay.updateResult(current_result);
-    }  
-};
-stateDrillDetailsDisplay.random = function(evt) {
-    stateDrillDetailsDisplay.update();
-       //Is a proper drill loaded else return
-   if (!stateDrillDetailsDisplay.isProperDrill()){
-        return;
-    }
-    stateDrillDetailsDisplay.play();   
-};
-stateDrillDetailsDisplay.isProperDrill = function(){
-    //Is a proper drill loaded else return
-    if (stateDrillDetailsDisplay.element.getAttribute('data-uuid') === "dummy"){
-        return false;
-    }
-    return true;
-};
-
-
-
-
-
-
-stateCalculations.nextCalculation = function(){
-    this.current_counter++;
-    if(this.current_counter >= this.maxCalculation){return false;}
-    return true;
-}
-stateCalculations.getCurrentCalculation = function(){
-    return {first:this.first[this.current_counter], second:this.second[this.current_counter]};
-}
-
-
-stateCalculations.isDrillOver = function() {
-    return !(this.current_counter < this.maxCalculation);
-};
-
-stateCalculations.getCurrentResult = function(){
-    let retResultFunction = this.operationsMap.get(stateDrillDetailsDisplay.operation.innerHTML);
-    let retResult = retResultFunction(this.first[this.current_counter], this.second[this.current_counter]);
-    return retResult;
-};
-stateCalculations.checkResult = function(){
-    if (parseInt(stateCalculationDisplay.result.innerHTML) === parseInt(this.getCurrentResult())){
-        //flash it in green
-        stateCalculationDisplay.result.classList.add("text-success");
-        setTimeout(function(){
-            if(!stateCalculations.nextCalculation()){
+            //Special case . & DEL
+            if(key === '.'){
+                console.log(".");
                 return;
             }
-            let currentCalc = stateCalculations.getCurrentCalculation();    
-            stateCalculationDisplay.updateFirst(currentCalc.first);
-            stateCalculationDisplay.updateSecond(currentCalc.second);
-            stateCalculationDisplay.updateResult('?');
-            stateCalculationDisplay.result.classList.remove("text-success");
-        }, 750);
+            if(key === 'DEL'){
+                
+                return;
+            }
+            //Process key
+            if( typeof(key) === 'number'){
+                keyNumber = key;
+                keyString = key.toString();
+            }
+            else if (typeof(key) === 'string'){
+                keyString = key;
+                keyNumber = parseInt(key);
+            }
 
-    }
-}
-
-
-//Drill Details
-
-
- 
- 
+            if(typeof(scd.result === 'number')){
+                resultString = resultString + keyString;
+                scd.result = resultString;
+            }
+            else if (typeof(scd.result === 'string')){
+                scd.result = scd.result + keyString;
+            }
+        },
+    });
+     
+});
